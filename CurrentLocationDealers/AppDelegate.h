a699dc31eee55f804af547f27ce0db9e9e16f344
SandyDhar <sudeepthiosdev@gmail.com>
2016-05-24 10:29:42 -0400

    Initial Commit

---
+++ CurrentLocationDealers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrentLocationDealers
+//
+//  Created by Sudeepth Dharavasthu on 5/24/16.
+//  Copyright © 2016 Sudeepth Dharavasthu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrentLocationDealers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrentLocationDealers
+//
+//  Created by Sudeepth Dharavasthu on 5/24/16.
+//  Copyright © 2016 Sudeepth Dharavasthu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
