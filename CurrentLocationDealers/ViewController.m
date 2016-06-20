d5a0e8c4251d98db8d09769d6fee4e351a92c6fb
SandyDhar <sudeepthiosdev@gmail.com>
2016-06-20 17:15:14 -0400

    appended postal code into url

--- CurrentLocationDealers/ViewController.m
+++ CurrentLocationDealers/ViewController.m
@@ -11,8 +11,8 @@
 
 @interface ViewController ()
 @property (nonatomic,strong)NSString *postalcode;
-@property (nonatomic,weak)NSDictionary *dealersDictionary;
-@property (nonatomic,weak)NSArray *dealerName;
+@property (nonatomic,strong)NSArray *dealersArray;
+@property (nonatomic,strong)NSMutableArray *dealerName;
 @property (nonatomic,weak)NSArray *dealerAddress;
 @property (nonatomic,weak)NSOperationQueue* queue;
 @property (nonatomic,weak)NSURLSessionDataTask *task;
@@ -33,8 +33,6 @@ CLPlacemark *placemark;
 {
     [super viewDidLoad];
     
-    // Do any additional setup after loading the view, typically from a nib.
-    
     locationManager = [[CLLocationManager alloc] init];
     
     geocoder = [[CLGeocoder alloc] init];
@@ -43,74 +41,72 @@ CLPlacemark *placemark;
     
     locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     
+    [locationManager requestAlwaysAuthorization];
+    
     [locationManager requestWhenInUseAuthorization];
-    
+   
     [locationManager startUpdatingLocation];
-    
+
     [self getAPIData];
-    
-    
-    
+   
 }
 
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateToLocations:(NSArray *)locations
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray<CLLocation *> *)locations
 {
     NSLog(@"didUpdateToLocation: %@", [locations lastObject]);
-    
     CLLocation *currentLocation = [locations lastObject];
     
-    /*
-     if (currentLocation != nil) {
-     _longitude = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
-     _latitude = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
-     }
-     */
-    
-   if (currentLocation != nil){
-    [locationManager stopUpdatingLocation];
+    if (currentLocation != nil)
+    {
+        [locationManager stopUpdatingLocation];
     }
-        else
-        {
-            NSLog(@"no cuurent location yet");
-        }
-    
-    
-    // Reverse Geocoding
+    else
+    {
+        NSLog(@"no cuurent location yet");
+    }
     
     NSLog(@"Resolving the Address");
     
-    [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error) {
-        
-        // NSLog(@"Found placemarks: %@, error: %@", placemarks, error);
-        if (error == nil && [placemarks count] > 0) {
-            placemark = [placemarks lastObject];
-            /*
-             self.textLabel.text = [NSString stringWithFormat:@"%@ %@\n%@ %@\n%@\n%@",
-             placemark.subThoroughfare, placemark.thoroughfare,
-             placemark.postalCode, placemark.locality,
-             placemark.administrativeArea,
-             placemark.country];
-             */
-            _postalcode = placemark.postalCode;
-            
-           
-            
-            NSLog(@"%@,%@,%@,%@",placemark.subThoroughfare,placemark.locality,placemark.country,placemark.postalCode);
-        } else {
-            NSLog(@"%@", error.debugDescription);
-        }
-    } ];
+    [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error)
+     {
+         // NSLog(@"Found placemarks: %@, error: %@", placemarks, error);
+         if (error == nil && [placemarks count] > 0)
+         {
+             placemark = [placemarks lastObject];
+             //NSLog(@"%@",placemark);
+             /*
+              self.textLabel.text = [NSString stringWithFormat:@"%@ %@\n%@ %@\n%@\n%@",
+              placemark.subThoroughfare, placemark.thoroughfare,
+              placemark.postalCode, placemark.locality,
+              placemark.administrativeArea,
+              placemark.country];
+              */
+              _postalcode = placemark.postalCode;
+              NSLog(@"%@,%@,%@,%@",placemark.subThoroughfare,placemark.locality,placemark.country,placemark.postalCode);
+         }
+         else
+         {
+             
+             NSLog(@"%@", error.debugDescription);
+             
+         }
+         
+     }
+     ];
+    
+
 }
 
 -(void)getAPIData{
     
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.edmunds.com/api/dealer/v2/dealers?zipcode=%@&radius=%@&fmt=json&api_key=ycwedw68qast829zx7sn9jnq",@"01609",@"10"]];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.edmunds.com/api/dealer/v2/dealers?zipcode=%@&radius=%@&fmt=json&api_key=ycwedw68qast829zx7sn9jnq",self.postalcode,@"30"]];
+    NSLog(@"%@",url);
     
     // attempt the request
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
-                                                           cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
-                                                       timeoutInterval:10];
+                                                        cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+                                                        timeoutInterval:10];
     [request setHTTPMethod: @"GET"];
     
     NSError *reqError;
@@ -123,16 +119,15 @@ CLPlacemark *placemark;
         
         NSDictionary *jsonData = [NSJSONSerialization JSONObjectWithData:data
                                                                  options:kNilOptions
-                                                                   error:NULL];
+                                                                 error:NULL];
+        self.dealersArray = jsonData[@"dealers"];
         
-        
-        
-        self.dealersDictionary = jsonData[@"dealers"];
-        
-        for (id item in self.dealersDictionary){
-            if (item[@"name"] != nil){
-                self.dealerName = item[@"name"];
-                NSLog(@"%@",self.dealerName);
+        self.dealerName = [[NSMutableArray alloc]init];
+        for (id item in self.dealersArray){
+            if (item[@"name"] != nil)
+            {
+                [self.dealerName addObject:item[@"name"]];
+               // NSLog(@"%@",self.dealerName);
             }
             else{
                 NSLog(@"could'nt find the names");
@@ -148,90 +143,77 @@ CLPlacemark *placemark;
             }
             
         }
-    
+        
     }
-
     
-//    NSURLSession *session = [NSURLSession sharedSession];
-//    self.task = [session dataTaskWithURL:[NSURL URLWithString:[NSString stringWithFormat:
-//                                                    @"https://api.edmunds.com/api/dealer/v2/dealers?zipcode=%@&radius=%@&fmt=json&api_key=ycwedw68qast829zx7sn9jnq",
-//                                                    @"01609",@"10"]]
-//      completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-//          
-//        
-//          
-//          if (data.length > 0 && error == nil)
-//          {
-//              
-//              NSDictionary *jsonData = [NSJSONSerialization JSONObjectWithData:data
-//                                                                       options:kNilOptions
-//                                                                         error:NULL];
-//              
-//              
-//              
-//              self.dealersDictionary = jsonData[@"dealers"];
-//              
-//              
-//              
-//              for (id item in self.dealersDictionary){
-//                  if (item[@"name"] != nil){
-//                      self.dealerName = item[@"name"];
-//                      NSLog(@"%@",self.dealerName);
-//                     }
-//                  else{
-//                      NSLog(@"could'nt find the names");
-//                  }
-//                  if (item[@"address"] != nil){
-//                      NSDictionary *address = item[@"address"];
-//                      self.dealerAddress = address[@"street"];
-//                      //NSLog(@"%@",self.dealerAddress);
-//                      // NSLog(@"%@",address[@"city"]);
-//                  }
-//                  else{
-//                      NSLog(@"could'nt find the address");
-//                  }
-//                  
-//              }
-//             // [self.tableView reloadData];
-//              
-//          }
-//          
-//      }
-//     ];
-//   
-//   [self.task resume]; 
+    
+    
+    //    NSURLSession *session = [NSURLSession sharedSession];
+    //    self.task = [session dataTaskWithURL:[NSURL URLWithString:[NSString stringWithFormat:
+    //                                                    @"https://api.edmunds.com/api/dealer/v2/dealers?zipcode=%@&radius=%@&fmt=json&api_key=ycwedw68qast829zx7sn9jnq",
+    //                                                    @"01609",@"10"]]
+    //      completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    //
+    //
+    //
+    //          if (data.length > 0 && error == nil)
+    //          {
+    //
+    //              NSDictionary *jsonData = [NSJSONSerialization JSONObjectWithData:data
+    //                                                                       options:kNilOptions
+    //                                                                         error:NULL];
+    //
+    //
+    //
+    //              self.dealersDictionary = jsonData[@"dealers"];
+    //
+    //
+    //
+    //              for (id item in self.dealersDictionary){
+    //                  if (item[@"name"] != nil){
+    //                      self.dealerName = item[@"name"];
+    //                      NSLog(@"%@",self.dealerName);
+    //                     }
+    //                  else{
+    //                      NSLog(@"could'nt find the names");
+    //                  }
+    //                  if (item[@"address"] != nil){
+    //                      NSDictionary *address = item[@"address"];
+    //                      self.dealerAddress = address[@"street"];
+    //                      //NSLog(@"%@",self.dealerAddress);
+    //                      // NSLog(@"%@",address[@"city"]);
+    //                  }
+    //                  else{
+    //                      NSLog(@"could'nt find the address");
+    //                  }
+    //
+    //              }
+    //             // [self.tableView reloadData];
+    //
+    //          }
+    //
+    //      }
+    //     ];
+    //
+    //   [self.task resume];
 }
-
 
 
 -(NSInteger)tableView:(UITableView *)tableview numberOfRowsInSection:(NSInteger)section
 {
-   // [self.task resume];
- //   NSLog(@"%ld sudeewwww",(long)_dealerName.count);
+    NSLog(@"%ld sudeewwww",(long)_dealerName.count);
     return self.dealerName.count;
-    //return 7;
 }
 
-    
+
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
 {
-        
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-        
-       // if (cell != nil){
-            
-            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
-            NSLog(@"%ld",(long)indexPath.row);
-    NSLog(@"%ld",(unsigned long)self.dealerName.count);
-            cell.textLabel.text = self.dealerName[indexPath.row];
-        //}
-    
-    
-    
-        
-            return cell;
-    
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    // NSLog(@"%ld",(long)indexPath.row);
+    // NSLog(@"%ld",(unsigned long)self.dealerName.count);
+    cell.textLabel.text = self.dealerName[indexPath.row];
+    return cell;
 }
 
 @end
